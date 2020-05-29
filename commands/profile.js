901d3b91f071251e965d860f0c67b0914ce27c70
ksaFROSTY <59263338+ksaFROSTY@users.noreply.github.com>
2020-05-29 23:53:27 +0300

    Create profile.js

---
+++ commands/profile.js
@@ -0,0 +1,4 @@
+const { Canvas } = require("canvas-constructor"); // You can't make images without this.
+const { resolve, join } = require("path"); // This is to get a font file.
+const { Attachment } = require("discord.js"); // This is to send the image via discord.
+const fetch = require("node-fetch"); // This is to fetch the user avatar and convert it to a buffer.
